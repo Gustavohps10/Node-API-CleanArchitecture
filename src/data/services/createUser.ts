8dfa7f0d39c9d140170f40d67a01e9eb383ba6d0
gustavo <gustavoh.santos735@gmail.com>
2022-08-24 14:07:32 -0300

    add - password encryption

--- src/data/services/createUser.ts
+++ src/data/services/createUser.ts
@@ -2,6 +2,7 @@ import { createUserUseCase } from "../../domain/useCases/createUser";
 import {UserRepository} from "../contracts/userRepository"
 import { UserDTO } from "../dto/userDTO";
 import * as EmailValidator from 'email-validator';
+import bcrypt from "bcrypt"
 
 export class CreateUserService implements createUserUseCase{
     
@@ -13,6 +14,8 @@ export class CreateUserService implements createUserUseCase{
         if(!EmailValidator.validate(userData.email)){
             throw new Error("Invalid email")
         }
+
+        userData.password = await bcrypt.hashSync(userData.password, 10)
         return this.userRepository.createUser(userData)
     }
 }
